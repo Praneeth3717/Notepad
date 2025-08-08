1e80dd70f035ee18c69d968df9dab1f7d3db2040
Praneeth3717 <144830750+Praneeth3717@users.noreply.github.com>
2025-08-09 03:13:56 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
  const FRONTEND_URL="https://notepad-flax.vercel.app"
- const BACKEND_URL="http://localhost:5000"
+ const BACKEND_URL="https://notepad-u68d.onrender.com"
 
  module.exports={FRONTEND_URL,BACKEND_URL}
